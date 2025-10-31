8c5c355b5d104073fe20eba43cd92b32032a5aaa
SapronovaAM <sokoliionok@yandex.ru>
2025-10-31 23:04:31 +0300

    Тестовое задание - Aston

---
+++ src/EvenNumbers.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Четные числа из массива");
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+}
---
+++ src/TwoNumberOperations.java
@@ -0,0 +1,66 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TwoNumberOperations {
+    public static void main(String[] args) {
+        boolean isActive = true;
+        try {
+            System.out.println("Введите а");
+
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+
+            System.out.println("Вы ввели a: " + a);
+
+            System.out.println("Введите b");
+            int b = scanner.nextInt();
+
+            System.out.println("Вы ввели b: " + b);
+
+            compareTwoNumbers(a, b);
+
+            addTwoNumbers(a, b);
+
+            subTwoNumbers(a, b);
+
+            divTwoNumbers(a, b);
+
+            mulTwoNumbers(a, b);
+
+            scanner.close();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Вы ввели не число");
+        }
+    }
+
+    public static void compareTwoNumbers(int a, int b) {
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a == b) {
+            System.out.println("a = b");
+        }  else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static void addTwoNumbers(int a, int b) {
+        int result = a + b;
+        System.out.println("a + b = " + result);
+    }
+
+    public static void subTwoNumbers(int a, int b) {
+        int result = a - b;
+        System.out.println("a - b = " + result);
+    }
+
+    public static void divTwoNumbers(int a, int b) {
+        int result = a / b;
+        System.out.println("a / b = " + result);
+    }
+
+    public static void mulTwoNumbers(int a, int b) {
+        int result = a * b;
+        System.out.println("a * b = " + result);
+    }
+}
